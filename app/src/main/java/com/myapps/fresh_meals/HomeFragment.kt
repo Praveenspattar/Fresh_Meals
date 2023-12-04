3059eb176145843cf66eeee96bd5e7e97f23c23e
Praveen <pattarpraveen15@gmail.com>
2023-12-05 00:36:00 +0530

    homeFrag Rv completed

--- app/src/main/java/com/myapps/fresh_meals/HomeFragment.kt
+++ app/src/main/java/com/myapps/fresh_meals/HomeFragment.kt
@@ -2,9 +2,14 @@ package com.myapps.fresh_meals
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.myapps.fresh_meals.Adapter.CategoryAdapter
 import com.myapps.fresh_meals.Api.Api_Interface
 import com.myapps.fresh_meals.Utils.constants
@@ -22,40 +27,33 @@ import com.myapps.fresh_meals.viewModel.viewModelFactory
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-//    lateinit var categoryAdapter : CategoryAdapter
     lateinit var viewModel: mealsViewModel
+    lateinit var recyclerView : RecyclerView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
 
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
 
         val api by lazy { constants.getRetrofitInstant().create(Api_Interface::class.java) }
         val repo = MealsRepository(api)
         val viewModelFactory = viewModelFactory(repo)
         viewModel = ViewModelProvider(this,viewModelFactory)[mealsViewModel::class.java]
 
-
-//        setupRecyclerview()
-
-        val recyclerView = binding.recyclerView
+        recyclerView = binding.recyclerView
         recyclerView.apply {
             layoutManager = GridLayoutManager(context,3)
         }
 
-        viewModel.liveDataCategory.observe(this) {
-//            categoryAdapter = CategoryAdapter(it.categories)
-            Log.d("theCategoryData","the data ${it.categories}")
-            recyclerView.adapter = CategoryAdapter(it.categories)
+        viewModel.liveDataCategory.observe(viewLifecycleOwner) {
+            val response = it.categories
+            Log.d("theCategoryData", "the data ${response}")
+            recyclerView.adapter = CategoryAdapter(response)
         }
+        return binding.root
 
     }
-
-//    fun setupRecyclerview(){
-//        binding.recyclerView.apply {
-//            adapter = categoryAdapter
-//            layoutManager = GridLayoutManager(context,3)
-//        }
-//    }
-
 }
